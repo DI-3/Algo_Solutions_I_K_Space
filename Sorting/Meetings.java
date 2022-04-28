efb631705a727781a9e353245a8331f517e280ac
Mithun Nagesh <311mitun@gmail.com>
2022-04-28 18:43:05 -0400

    Update Meetings.java

--- Sorting/Meetings.java
+++ Sorting/Meetings.java
@@ -3,19 +3,23 @@
     Complete the function below.
     */
     public static int can_attend_all_meetings(List<List<Integer>> intervals) {
-        int prev_end = 0;
-        int prev_start = 0;
+       //  Sort the meetings by the Start time
+       //  So a person will be able to attend all the mmeting if
+       //. essentially the start time of the second meeting >= end time of previous in the sorted array 
+       
+       // one thing is can we have the same interval twice if so then we would not be able to attend all meetings
+        intervals.sort((x, y) -> Integer.compare(x.get(0), y.get(0)));
         
-        for(List<Integer> interval : intervals ){
+        List<Integer> prevInterval = intervals.get(0);
+        for (int i = 1; i < intervals.size(); i++){
+            List<Integer> currInterval = intervals.get(i);
             
-            if(interval.get(0) > interval.get(1))
-                return 0;
-            else if(prev_end > interval.get(0) && prev_start < interval.get(1))
-                return 0;
-            
-            prev_end = interval.get(1);
-            prev_start = interval.get(0);
+            if (currInterval.get(0) < prevInterval.get(1)){
+                    return 0;
+            }
+            prevInterval = currInterval;
         }
         
         return 1;
+        
     }
